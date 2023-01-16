5246bac0107b9efadc33054bfe466022d4ab29a3
Chimplement <19803870+Chimplement@users.noreply.github.com>
2023-01-16 11:46:41 +0000

    Deploying to gh-pages from @ Travh98/GWJ_Jan2023_KeyboardThongs@3587662770ed3f8a8d1f3e50d1ab723ab407cc32 🚀

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1673869259|2107527";
+const CACHE_VERSION = "1673869597|2367631";
 const CACHE_PREFIX = "godot wild jam 5-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
