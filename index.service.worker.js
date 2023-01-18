935767710a71f20e6d17dd3fc2ebe6631e57dd63
Zion-D-Hill <zionhill2004@gmail.com>
2023-01-18 14:23:57 +0000

    Deploying to gh-pages from @ Travh98/GWJ_Jan2023_Heartless@8fa802ad647c03c4385f6630580feb8050dfb00f 🚀

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1674050955|2161484";
+const CACHE_VERSION = "1674051833|2475591";
 const CACHE_PREFIX = "godot wild jam 5-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
