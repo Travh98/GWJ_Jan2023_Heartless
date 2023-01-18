b3d3eb74ec866b7f6a899d13882ddd0ca4b39c81
Travh98 <travishunter33@gmail.com>
2023-01-17 21:22:46 -0500

    Added attack cooldown

--- actors/player_character/player_character.gd
+++ actors/player_character/player_character.gd
@@ -4,17 +4,25 @@ class_name PlayerCharacter
 const ANIMATION_DIRECTIONS := {Vector2.RIGHT: "right", Vector2.LEFT: "left", Vector2.UP: "up", Vector2.DOWN: "down"}
 const SPEED := 128
 var temporary_speed
-var attack_range = 35
 
+# Potentially modifyable stats
+var attack_range = 35
+var attack_cooldown_secs = 0.5
+
+
+var attack_ready : bool = true
 var velocity := Vector2.ZERO
 
 onready var animated_sprite: AnimatedSprite = $Sprite
 onready var axe_attack_scene = load("res://actors/axe_swing_attack.tscn")
 
 onready var dash_timer : Timer = get_node("DashTimer")
+onready var attack_cooldown_timer : Timer = get_node("AttackCooldownTimer")
 
 func _ready():
 	dash_timer.connect("timeout", self, "dash_finished")
+	attack_cooldown_timer.connect("timeout", self, "attack_cooled_down")
+	attack_cooldown_timer.wait_time = attack_cooldown_secs
 	temporary_speed = SPEED
 
 func _physics_process(delta: float) -> void:
@@ -25,16 +33,14 @@ func _physics_process(delta: float) -> void:
 
 	move_and_slide(temporary_speed * input_direction)
 	
-	if Input.is_action_just_pressed("attack"):
+	if Input.is_action_just_pressed("attack") and attack_ready:
 		axe_attack()
 	if Input.is_action_just_pressed("dash"):
 		dash()
 
-
 func _process(delta: float) -> void:
 	var mouse_direction := Vector2.ZERO.direction_to(get_local_mouse_position())
 	handle_mouse_direction(mouse_direction)
-
 
 func handle_mouse_direction(mouse_direction: Vector2) -> void:
 	if mouse_direction.x > 0.0:
@@ -46,7 +52,6 @@ func handle_mouse_direction(mouse_direction: Vector2) -> void:
 	if mouse_direction.y < -0.85:
 		_update_sprite(Vector2.UP)
 
-
 func _update_sprite(direction: Vector2) -> void:
 	animated_sprite.animation = ANIMATION_DIRECTIONS[direction]
 
@@ -57,9 +62,15 @@ func axe_attack() -> void:
 	attack.rotation = get_angle_to(get_global_mouse_position())
 	attack.global_transform.origin += Vector2(attack_range, 0).rotated(attack.rotation)
 	
+	attack_ready = false
+	attack_cooldown_timer.start()
+	
 func dash() -> void:
 	temporary_speed = SPEED * 5
 	dash_timer.start()
 	
 func dash_finished() -> void:
 	temporary_speed = SPEED
+	
+func attack_cooled_down() -> void:
+	attack_ready = true
