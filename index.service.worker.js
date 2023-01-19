d07ec9b137fe2b055655fc9cf8dd0a481f3fc5ca
Travh98 <36664554+Travh98@users.noreply.github.com>
2023-01-19 01:18:18 +0000

    Deploying to gh-pages from @ Travh98/GWJ_Jan2023_Heartless@9a4c5f24317782b4b878bd5e63112cfee2c22fb8 🚀

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,8 +3,8 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1674058667|2085996";
-const CACHE_PREFIX = "godot wild jam 5-sw-cache-";
+const CACHE_VERSION = "1674091094|2934791";
+const CACHE_PREFIX = "Heartless-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
 // Files that will be cached on load.
