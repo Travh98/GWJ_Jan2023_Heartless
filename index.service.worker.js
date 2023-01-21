aaa6b88896a179adf3c327703c676e0f1cda6c75
RaoulMarz <raoulmarais@gmail.com>
2023-01-21 19:56:50 +0000

    Deploying to gh-pages from @ Travh98/GWJ_Jan2023_Heartless@60a1848de49b3263b6c61f01bd781f71321b110a 🚀

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1674298829|2559239";
+const CACHE_VERSION = "1674331006|2946319";
 const CACHE_PREFIX = "Heartless-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
